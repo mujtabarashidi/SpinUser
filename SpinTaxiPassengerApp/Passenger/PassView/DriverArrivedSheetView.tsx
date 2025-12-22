0c73d263532f008d6440c38d4274ba63e2de9cd6
Spin <mujtabar93@gmail.com>
2025-12-22 18:23:07 +0100

    Update submodules and cleanup

--- SpinTaxiPassengerApp/Passenger/PassView/DriverArrivedSheetView.tsx
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-export default function DriverArrivedSheetView() {
-  return null;
-}
--- SpinTaxiPassengerApp/Passenger/PassView/DriverNoteView.tsx
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-export default function DriverNoteView() {
-  return null;
-}
